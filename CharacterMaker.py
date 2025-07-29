84a73f3da64439ee7733f79ee34dfd33f1a8d91c
Steven Casperson <steven.casperson7@gmail.com>
2025-07-29 11:32:29 -0700

    Create CharacterMaker.py

---
+++ CharacterMaker.py
@@ -0,0 +1,18 @@
+/// The data that the player selects, based on choices presented to screen
+set player.count == 1;
+set player.level == 1;
+set player.race == [elf, dwarf, gnome, halfling, human];
+set player.class == [fighter, cleric, rogue, magic-user]
+
+set player.strength = 9;
+set player.dexterity = 9;
+set player.intelligence = 9;
+set player.wisdom = 9;
+set player.consitution = 9;
+set player.charisma = 9;
+set player.experience = 0;
+set player.equipment == [];
+set player.spells == [];
+set player.skills == [];
+
+
